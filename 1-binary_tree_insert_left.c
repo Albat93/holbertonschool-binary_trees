08be833252194846d95b64541e0772f3473fd8a1
Albat93 <albat93120@gmail.com>
2024-12-09 14:22:06 +0100

    task 1 binary insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,8 +21,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	newnode->parent = parent;
 	newnode->n = value;
-	node->left = NULL;
-	node->right = NULL;
+	newnode->left = NULL;
+	newnode->right = NULL;
 
 	if (parent->left != NULL)
 	{
