3f09c00fc3a0c064d287aef7d90f5e494639327c
Albat93 <albat93120@gmail.com>
2024-12-10 15:14:19 +0100

    fix for output

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,22 +9,23 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t x, y;
-	if (tree != NULL)
+
+	if (tree ==  NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	x = binary_tree_height(tree->left);
+	y = binary_tree_height(tree->right);
+
+	if (x >= y)
 	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (0);
-
-		x = binary_tree_height(tree->left);
-		y = binary_tree_height(tree->right);
-
-		if (x <= y)
-		{
-			return (1 + x);
-		}
-		else
-		{
-			return (1 + y);
-		}
+		return (x + 1);
 	}
-	return (0);
+	else
+	{
+		return (y + 1);
+	}
+
 }
