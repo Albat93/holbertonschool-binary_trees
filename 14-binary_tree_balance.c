32943eb42a7d0d1db53a4bbde7032432aa419e14
Albat93 <albat93120@gmail.com>
2024-12-10 18:24:20 +0100

    add file binary height and modify value for left and right with previous function to call it back

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,30 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ *@tree: the root of node
+ *Return: 0 if Null
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree ==  NULL)
+		return (0);
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	if (left >= right)
+	{
+		return (1 + left);
+	}
+
+	return (1 + right);
+
+}
+
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the size
@@ -8,16 +33,13 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left, right;
+	int left, right;
 
 	if (tree == NULL)
 		return (0);
 
-	if (binary_tree_height(tree))
-		return (1);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
-	left = binary_tree_balance(tree->left);
-	right = binary_tree_balance(tree->right);
-
-	return (right - left);
+	return (left - right);
 }
